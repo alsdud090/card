79ee1417369cd84b5aeb628deefe0eef573cd944
JMY <alsdud010904@hotmail.com>
2025-06-26 05:59:57 +0900

    디코 아이디 수정

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-const userID = "738748102311280681";
+const userID = "256970019089547267";
 const elements = {
 	statusBox: document.querySelector(".status"),
 	statusImage: document.getElementById("status-image"),
